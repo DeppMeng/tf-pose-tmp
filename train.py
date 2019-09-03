d66dc312be93e3b7c5381aff2d0e85cdcfea73f2
Depu Meng (FA Talent) <v-depme@microsoft.com>
2019-09-03 19:22:04 +0800

    fixing bugs in changed the config style and add the hr_rnet w20 config

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import argparse
 import importlib.util
 import numpy as np
 
-# from config import cfg
+from config.hr_rnet_w20_bs128_256x192_epoch210 import cfg
 from core.engine import Trainer
 from core.model import Model
 from tfflat.utils import mem_info
@@ -22,8 +22,8 @@ def parse_args():
 
 
 args = parse_args()
-cfg = importlib.import_module('config.{}'.format(args.cfg))
-cfg = cfg.cfg
+# cfg = importlib.import_module('config.{}'.format(args.cfg))
+# cfg = cfg.cfg
 cfg.set_args(args.gpu_ids, args.continue_train)
 trainer = Trainer(Model(cfg), cfg)
 trainer.train()
